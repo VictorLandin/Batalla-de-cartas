d9f90fcfd1ff2ce5a1d8df787958c7d8b70cbb8a
VictorLandin <victor.landin.racineux@gmail.com>
2023-04-13 14:16:09 +0200

    commit inicial

---
+++ Batalla de cartas/src/Model/Acciones.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author a22victorlr
+ */
+public class Acciones {
+    //esta clase registrará que hace cada acción segun su ID, Separados por enemigo, tambien marcará que efectos hacen, si hacen
+}
---
+++ Batalla de cartas/src/Model/Efectos.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author a22victorlr
+ */
+public class Efectos {
+    //esta clase registra que efectos has segun su ID, deversas cartas o acciones generaran efectos
+}
